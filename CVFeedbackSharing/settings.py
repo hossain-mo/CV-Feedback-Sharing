6d40b2e02d7db0e89463c4994c8cbb0658c5ca35
hossain-mo <hossainfcih@gmail.com>
2020-09-23 00:30:30 +0200

    add static root to deploy in heroku

--- CVFeedbackSharing/settings.py
+++ CVFeedbackSharing/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/dev/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
-
+import os
 from pathlib import Path
 import datetime
 
@@ -152,3 +152,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
